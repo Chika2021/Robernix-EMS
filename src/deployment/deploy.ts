9dfe72989750e8b9b4658626698a8745b1c82bad
Chika2021 <amaechichika9@gmail.com>
2025-09-12 02:07:16 -0700

    Robernix EMR

--- src/deployment/deploy.ts
+++ src/deployment/deploy.ts
@@ -1,1 +1,4 @@
-export const apiUrl = 'http://localhost:3000'; // Change to production URL when deploying       
+// export const apiUrl = 'http://localhost:3000'; // Change to production URL when deploying   
+
+export const apiUrl = 'https://rwork.fly.dev'; // Change to production URL when deploying
+// export const apiUrl = 'https://rwork.onrender.com/api'; // Change to production URL when deploying
